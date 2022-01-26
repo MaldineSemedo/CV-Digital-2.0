f728489e5f893b90cef99de8113a3d52e71ed8bd
MaldineSemedo <paulomaldine12@gmail.com>
2022-01-26 17:24:09 -0100

    Versao login 3.1

--- cv-digital/src/App.js
+++ cv-digital/src/App.js
@@ -6,11 +6,11 @@ const signInButton = document.getElementById('signIn');
 const container = document.getElementById('container');
 
 signUpButton.addEventListener('click', () => {
-	container.classList.add("right-panel-active");
+container.classList.add("right-panel-active");
 });
 
 signInButton.addEventListener('click', () => {
-	container.classList.remove("right-panel-active");
+container.classList.remove("right-panel-active");
 });
 
 function App() {
@@ -37,45 +37,45 @@ function App() {
 	<div class="container" id="container">
 	<div class="form-container sign-up-container">
 		<form action="#">
-			<h1>Create Account</h1>
+			<h1>Criar uma conta</h1>
 			<div class="social-container">
 				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
 				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
 				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
 			</div>
-			<span>or use your email for registration</span>
-			<input type="text" placeholder="Name" />
-			<input type="email" placeholder="Email" />
-			<input type="password" placeholder="Password" />
-			<button>Sign Up</button>
+			<span>ou use seu e-mail para cadastro</span>
+			<input type="text" placeholder="Nome" />
+			<input type="email" placeholder="E-mail" />
+			<input type="password" placeholder="Senha" />
+			<button>INCREVER - SE</button>
 		</form>
 	</div>
 	<div class="form-container sign-in-container">
 		<form action="#">
-			<h1>Sign in</h1>
+			<h1>Entrar</h1>
 			<div class="social-container">
 				<a href="#" class="social"><i class="fab fa-facebook-f"></i></a>
 				<a href="#" class="social"><i class="fab fa-google-plus-g"></i></a>
 				<a href="#" class="social"><i class="fab fa-linkedin-in"></i></a>
 			</div>
-			<span>or use your account</span>
-			<input type="email" placeholder="Email" />
-			<input type="password" placeholder="Password" />
-			<a href="#">Forgot your password?</a>
-			<button>Sign In</button>
+			<span>ou use sua conta</span>
+			<input type="email" placeholder="E-mail" />
+			<input type="password" placeholder="Senha" />
+			<a href="#">Esqueceu sua senha?</a>
+			<button>ENTRAR</button>
 		</form>
 	</div>
 	<div class="overlay-container">
 		<div class="overlay">
 			<div class="overlay-panel overlay-left">
-				<h1>Welcome Back!</h1>
-				<p>To keep connected with us please login with your personal info</p>
-				<button class="ghost" id="signIn">Sign In</button>
+				<h1>Bem vindo de volta!</h1>
+				<p>Para se manter conectado conosco, faça login com suas informações pessoais</p>
+				<button class="ghost" id="signIn">ENTRAR</button>
 			</div>
 			<div class="overlay-panel overlay-right">
-				<h1>Hello, Friend!</h1>
-				<p>Enter your personal details and start journey with us</p>
-				<button class="ghost" id="signUp">Sign Up</button>
+				<h1>Olá amigo Afi!</h1>
+				<p>Insira seus dados pessoais e comece a jornada conosco</p>
+				<button class="ghost" id="signUp">INSCREVER - SE</button>
 			</div>
 		</div>
 	</div>
